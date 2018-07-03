b15cc9914dad4f155cde5ea863e60cedeca781e6
Lindsay Ortiz <lindsay.ortiz@live.com>
2018-07-03 22:16:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,3 +38,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
 }
+
+function removeElementFromEndOfArray(array) {
+  var returnArray = array.slice(0, array.length - 1);
+  return returnArray;
+}
