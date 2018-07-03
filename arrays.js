7e661cb54e991e943c8a65224f4a880dc7aa12f8
Lindsay Ortiz <lindsay.ortiz@live.com>
2018-07-03 22:14:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
 }
+
+function removeElementFromBeginningOfArray(array) {
+  retunArray = array.slice(1);
+  return retunArray;
+}
